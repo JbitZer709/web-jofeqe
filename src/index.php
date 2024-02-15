caf3ab72d07a0d427f19677362f8498492f0dc25
ske_fall <KILLERSALSYER1234@hotmail.com>
2024-02-14 21:30:03 -0600

    commit

---
+++ src/index.php
@@ -0,0 +1,5 @@
+<div aling='center'>
+  Juan Jose Gonzalez Cruz <p>4°Q</p>
+</div>
+
+<a href="#" target='_blank'>Diagrama de base de datos</a>
--- src/main.js
+++
@@ -1,1 +0,0 @@
-console.log('Si funciono correctamente')
